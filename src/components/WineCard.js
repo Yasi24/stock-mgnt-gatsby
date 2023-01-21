947a9e2ff1901e222aa4d7bc4f9a1dd62224d339
Ahmad Yasi Faizi <yasifaizi10@gmail.com>
2023-01-21 22:01:00 +0700

    sdf

--- src/components/WineCard.js
+++ src/components/WineCard.js
@@ -3,6 +3,12 @@ import { Card, Button } from 'react-bootstrap';
 function WineCard(props) {
     const { title, image, price, handleClick } = props;
     return (
-        <Card style={{ width: '18rem' }}>            <Card.Img variant="top" src={image} />            <Card.Body>                <Card.Title>{title} - {price} Baht</Card.Title>                <Button variant="primary" onClick={handleClick}>Add to Cart</Button>            </Card.Body>        </Card>    )
+        <Card style={{ width: '18rem' }}>            
+        <Card.Img variant="top" src={image} />            
+        <Card.Body>                
+            <Card.Title>{title} - {price} Baht</Card.Title>                
+            <Button variant="primary" onClick={handleClick}>Add to Cart</Button>            
+            </Card.Body>        
+            </Card>    )
 }
 export default WineCard
